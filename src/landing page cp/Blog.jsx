ea25bd8a734d046fd6885183efbced7e654a13cc
Ebube <nwokegodwin45@gmail.com>
2023-12-11 01:32:21 -0800

    fix blog component axios get call

--- src/landing page cp/Blog.jsx
+++ src/landing page cp/Blog.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Article from "./Article";
 import styles from "../style";
 import ReactLoading from "react-loading";
+import axios from "axios";
 
 const Blog = () => {
   const [items, setItems] = useState([]);
@@ -9,12 +10,12 @@ const Blog = () => {
 
   useEffect(() => {
     if (loading) {
-      fetch(
-        "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=400c57761b2e4bc3842e8865c25ddb60"
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          setItems(data.articles);
+      axios
+        .get(
+          "https://newsapi.org/v2/everything?q=cryptocurrency+bitcoin+ethereum&sortBy=top&apiKey=400c57761b2e4bc3842e8865c25ddb60"
+        )
+        .then((res) => {
+          setItems(res.data.articles);
           setLoading(false);
         })
         .catch((err) => {
@@ -33,22 +34,13 @@ const Blog = () => {
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          {loading ? (
-            <div className="spinner-div">
-              <ReactLoading
-                type={"spinningBubbles"}
-                color={"#1CEEEB"}
-                height={20}
-                width={20}
-              />
-            </div>
-          ) : (
-            items
-              .filter((item, index) => index < 1)
-              .map((filteredItem, i) => (
-                <Article items={filteredItem} key={i} />
-              ))
-          )}
+          {loading
+            ? " "
+            : items
+                .filter((item, index) => index < 1)
+                .map((filteredItem, i) => (
+                  <Article items={filteredItem} key={i} />
+                ))}
         </div>
         <div className="gpt3__blog-container_groupB">
           {loading ? (
