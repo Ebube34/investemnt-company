bb07701745ab80886c50fd3d4708a6544714334f
Ebube <nwokegodwin45@gmail.com>
2023-12-10 20:07:10 -0800

    fix blog component loading state

--- src/landing page cp/Blog.jsx
+++ src/landing page cp/Blog.jsx
@@ -1,33 +1,76 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Article from "./Article";
-// import { articleImg01, articleImg02, articleImg03, articleImg04, articuleImg05 } from '../assets';
 import styles from "../style";
+import ReactLoading from "react-loading";
 
 const Blog = () => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  
   useEffect(() => {
-    fetch("https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=400c57761b2e4bc3842e8865c25ddb60")
-    .then(res => res.json())
-    .then(data => setItems(data.articles))
-  }, [])
+    if (loading) {
+      fetch(
+        "https://newsapi.org/v2/everything?q=cryptocurrency&apiKey=400c57761b2e4bc3842e8865c25ddb60"
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          setItems(data.articles);
+          setLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setLoading(true);
+        });
+    }
+  }, [loading]);
 
-  return  (
+  return (
     <div className="gpt3__blog" id="blog">
       <div className="gpt3__blog-heading">
-        <h2 className={styles.heading2} style={{ fontWeight: "600"}}>Latest news <br /> </h2>
+        <h2 className={styles.heading2} style={{ fontWeight: "600" }}>
+          Latest news <br />{" "}
+        </h2>
       </div>
       <div className="gpt3__blog-container">
         <div className="gpt3__blog-container_groupA">
-          {items.filter((item, index) => index < 1).map((filteredItem, i) => <Article items={filteredItem} key={i}/> )}
+          {loading ? (
+            <div className="spinner-div">
+              <ReactLoading
+                type={"spinningBubbles"}
+                color={"#1CEEEB"}
+                height={20}
+                width={20}
+              />
+            </div>
+          ) : (
+            items
+              .filter((item, index) => index < 1)
+              .map((filteredItem, i) => (
+                <Article items={filteredItem} key={i} />
+              ))
+          )}
         </div>
         <div className="gpt3__blog-container_groupB">
-          {items.filter((item, index) => index > 1 && index < 6).map((filteredItem, index) => <Article items={filteredItem} key={index} /> )}
+          {loading ? (
+            <div className="spinner-div">
+              <ReactLoading
+                type={"spinningBubbles"}
+                color={"#1CEEEB"}
+                height={20}
+                width={20}
+              />
+            </div>
+          ) : (
+            items
+              .filter((item, index) => index > 1 && index < 6)
+              .map((filteredItem, index) => (
+                <Article items={filteredItem} key={index} />
+              ))
+          )}
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Blog;
