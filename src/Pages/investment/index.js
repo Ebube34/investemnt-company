edac9a612766d5d6d924d7522681a9996de745f4
Ebube <nwokegodwin45@gmail.com>
2023-12-29 04:50:56 -0800

    fix changes to invest page

--- src/Pages/investment/index.js
+++ src/Pages/investment/index.js
@@ -6,18 +6,14 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Typography from "@mui/material/Typography";
 import { safeAndSecure, balancedPortfolio } from "../../assets";
 import { Button } from "../../landing page cp";
-import { step01, step02, step03 } from "../../assets";
 
 const Invest = () => {
   const colors = ["#0088FE", "#00C49F", "#FFBB28"];
   const [index, setIndex] = useState(0);
   const delay = 25000;
   const timeoutRef = useRef(null);
-
-
 
   function resetTimeout() {
     if (timeoutRef.current) {
@@ -39,7 +35,7 @@ const Invest = () => {
     return () => {
       resetTimeout();
     };
-  }, [index, ]);
+  }, [index]);
   return (
     <>
       <div className="bg-primary w-full overflow-hidden">
@@ -120,13 +116,30 @@ const Invest = () => {
               }}
             >
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h5">Fractional Shares</Typography>
+                <h1 className={styles.heading4}>Fractional Shares</h1>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  Benefit from exposure to an ETF or stock with a higher price
-                  per share
-                </Typography>
+                <p className={styles.paragraph}>
+                  Maximize your investment potential by gaining exposure to an
+                  ETF or stock with a higher price per share. This approach
+                  offers several advantages. Firstly, it allows you to tap into
+                  the potential growth of established and high-performing
+                  assets. Additionally, investing in higher-priced shares often
+                  aligns with companies that have demonstrated sustained success
+                  and stability. Furthermore, exposure to stocks or ETFs with a
+                  higher price per share can contribute to a well-rounded and
+                  diversified portfolio. It introduces a layer of sophistication
+                  to your investment strategy, as these assets are typically
+                  associated with companies that have a strong market presence
+                  and a track record of delivering value to shareholders. While
+                  it's essential to assess and manage risk, incorporating such
+                  assets into your portfolio can offer a unique avenue for
+                  capital appreciation. Keep in mind that diversification
+                  remains a key principle in effective portfolio management, and
+                  a balanced approach that includes exposure to higher-priced
+                  shares can contribute to the overall resilience and
+                  performance of your investment portfolio.
+                </p>
               </AccordionDetails>
             </Accordion>
             <Accordion
@@ -137,13 +150,35 @@ const Invest = () => {
               }}
             >
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h5">Automatic rebalancing</Typography>
+                <h1 className={styles.heading4}>Automatic rebalancing</h1>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  enjoy investment portfolio rebalancing as frequently as the
-                  market changes
-                </Typography>
+                <p className={styles.paragraph}>
+                  Embrace the dynamic nature of the financial landscape by
+                  relishing the opportunity to engage in investment portfolio
+                  rebalancing as frequently as the market undergoes changes.
+                  This proactive approach to managing your portfolio offers a
+                  multitude of benefits. Firstly, frequent rebalancing allows
+                  you to stay aligned with your financial goals and risk
+                  tolerance. As market conditions evolve, the weightings of your
+                  assets may shift, impacting the overall risk and return
+                  profile of your portfolio. By regularly reassessing and
+                  adjusting your allocations, you can ensure that your
+                  investment strategy remains in harmony with your objectives.
+                  Moreover, taking joy in the process of portfolio rebalancing
+                  empowers you to capitalize on market trends and capitalize on
+                  emerging opportunities. It enables you to buy low and sell
+                  high, optimizing your asset allocation based on the prevailing
+                  market conditions. Additionally, embracing the art of frequent
+                  rebalancing fosters a sense of financial agility. It positions
+                  you to adapt swiftly to changing economic landscapes,
+                  geopolitical events, and other external factors that may
+                  influence the performance of your investments. In essence,
+                  viewing investment portfolio rebalancing as an enjoyable and
+                  integral part of your financial journey enhances your ability
+                  to navigate the complexities of the market and maintain a
+                  resilient and well-optimized investment portfolio.
+                </p>
               </AccordionDetails>
             </Accordion>
             <Accordion
@@ -154,12 +189,36 @@ const Invest = () => {
               }}
             >
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h5">Simple Access</Typography>
+                <h1 className={styles.heading4}>Simple Access</h1>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  Manage your accounts anytime and from anywhere
-                </Typography>
+                <p className={styles.paragraph}>
+                  Gain unparalleled control over your financial landscape by
+                  effortlessly managing your accounts anytime and from anywhere.
+                  This level of accessibility is a game-changer in today's
+                  fast-paced world, offering you a host of advantages. The
+                  ability to manage your accounts around the clock empowers you
+                  with real-time insights into your financial standing. Whether
+                  you're at home, on the go, or traveling internationally,
+                  staying connected to your accounts ensures that you are
+                  well-informed and in control of your financial decisions.
+                  Moreover, the convenience of managing your accounts from any
+                  location fosters a proactive and responsive financial
+                  approach. You can seize opportunities, address concerns, and
+                  make strategic adjustments to your portfolio promptly,
+                  maximizing the potential for financial growth and stability.
+                  This flexibility also contributes to a seamless and
+                  stress-free financial experience. By eliminating geographical
+                  constraints, you can conduct transactions, monitor
+                  investments, and stay on top of your financial goals with
+                  ease. The peace of mind that comes with knowing you can manage
+                  your accounts whenever and wherever allows you to focus on
+                  what matters most to you. In essence, the power to manage your
+                  accounts at your fingertips not only enhances your financial
+                  management capabilities but also aligns with the dynamic and
+                  interconnected nature of modern life, providing you with the
+                  freedom to shape your financial destiny on your terms.
+                </p>
               </AccordionDetails>
             </Accordion>
             <Accordion
@@ -170,13 +229,36 @@ const Invest = () => {
               }}
             >
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h5">Dividend Reinvestment</Typography>
+                <h1 className={styles.heading4}>Dividend Reinvestment</h1>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  We reinvest your stock dividends back into your investment
-                  portfolio so you can earn more
-                </Typography>
+                <p className={styles.paragraph}>
+                  Experience the compounding magic of your investments through
+                  our innovative approach: we automatically reinvest your stock
+                  dividends back into your investment portfolio, amplifying your
+                  earning potential. Dividend reinvestment is a strategic move
+                  that can significantly enhance the overall performance of your
+                  portfolio over time. By seamlessly reinvesting the dividends
+                  generated by your stocks, you capitalize on the power of
+                  compounding, where your earnings generate additional earnings.
+                  This compounding effect can lead to exponential growth,
+                  allowing you to harness the full potential of your
+                  investments. This hands-free reinvestment process not only
+                  optimizes your returns but also aligns with a long-term
+                  wealth-building strategy. It reflects a commitment to
+                  maximizing the value of your investments and ensuring that
+                  your portfolio is working diligently to create wealth on your
+                  behalf. Furthermore, by automatically reinvesting dividends,
+                  you eliminate the need for manual intervention, making your
+                  investment journey more convenient and efficient. It's a
+                  set-and-forget approach that empowers you to focus on your
+                  broader financial goals while your portfolio works diligently
+                  to generate wealth through the power of reinvested dividends.
+                  In essence, our dividend reinvestment feature is designed to
+                  transform your investment experience by turning regular
+                  payouts into a dynamic force propelling your portfolio toward
+                  sustained and robust financial growth.
+                </p>
               </AccordionDetails>
             </Accordion>
             <Accordion
@@ -187,12 +269,40 @@ const Invest = () => {
               }}
             >
               <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography variant="h5">Compound Interest</Typography>
+                <h1 className={styles.heading4}>Compound Interest</h1>
               </AccordionSummary>
               <AccordionDetails>
-                <Typography>
-                  Grow your money with minimum effort through compound interest
-                </Typography>
+                <p className={styles.paragraph}>
+                  Unlock the potential for effortless wealth accumulation by
+                  harnessing the remarkable power of compound interest. This
+                  financial phenomenon has the ability to grow your money
+                  exponentially over time, requiring minimal effort on your
+                  part. At its core, compound interest involves not only earning
+                  interest on your initial investment but also on the
+                  accumulated interest that has been added to your principal
+                  over time. This compounding effect creates a snowball effect,
+                  allowing your money to multiply at an accelerating rate. One
+                  of the key advantages of compound interest is the ability to
+                  amplify the impact of your investments without the need for
+                  active management. As your earnings are reinvested and
+                  contribute to the growth of your principal, the compounding
+                  process takes on a life of its own, steadily building a robust
+                  foundation for your financial future. Embracing the concept of
+                  compound interest aligns with a patient and long-term approach
+                  to wealth building. It emphasizes the importance of staying
+                  invested and allowing time to work in your favor. The longer
+                  your money has to compound, the greater the potential for
+                  substantial growth. Moreover, the beauty of compound interest
+                  lies in its ability to transform modest contributions into
+                  significant wealth over time. Even small, consistent
+                  investments can blossom into a substantial financial nest egg,
+                  showcasing the accessibility and inclusivity of this powerful
+                  financial tool. In essence, growing your money through
+                  compound interest is a smart and effortless way to build
+                  wealth. By understanding and harnessing the dynamics of
+                  compounding, you set the stage for a financially secure and
+                  prosperous future with the least amount of active effort.
+                </p>
               </AccordionDetails>
             </Accordion>
           </div>
@@ -276,21 +386,10 @@ const Invest = () => {
               className="slideshowSlider"
             >
               <div
-                style={{ paddingTop: "7rem", paddingBottom: "1rem" }}
+                style={{ paddingTop: "4rem", paddingBottom: "1rem" }}
                 className="slide"
               >
                 <div>
-                  <img
-                    alt="profile-user"
-                    width="90px"
-                    height="90px"
-                    src={step01}
-                    style={{
-                      borderRadius: "50%",
-                      marginLeft: "47%",
-                      paddingBottom: "1rem",
-                    }}
-                  />
                   <h1
                     style={{ paddingBottom: "2rem" }}
                     className={styles.heading4}
@@ -301,29 +400,17 @@ const Invest = () => {
                     style={{ paddingBottom: "2rem" }}
                     className={styles.paragraph}
                   >
-                    Click the open account button and fill the form right away
+                    Click the open account <br /> button and fill the form right
+                    away <br />
                     to have an account opened for you. <br /> After you open
-                    your account, the next thing is to select a plan.
+                    your account,
+                    <br /> the next thing is to <br /> select a plan.
                   </p>
                 </div>
                 <Button content="Open account"></Button>
               </div>
-              <div
-                style={{ paddingTop: "2rem", paddingBottom: "1rem" }}
-                className="slide"
-              >
+              <div style={{ paddingBottom: "1rem" }} className="slide">
                 <div>
-                  <img
-                    alt="profile-user"
-                    width="90px"
-                    height="90px"
-                    src={step02}
-                    style={{
-                      borderRadius: "50%",
-                      marginLeft: "47%",
-                      paddingBottom: "1rem",
-                    }}
-                  />
                   <h1
                     style={{ paddingBottom: "2rem" }}
                     className={styles.heading4}
@@ -332,31 +419,19 @@ const Invest = () => {
                   </h1>
 
                   <p className={styles.paragraph}>
-                    After opening your free account, the next thing is to select
-                    a plan that matches your budget, as well as your financial
-                    objectives. <br /> If you don't find a plan that suits your,
-                    just reach out to one of our available advisors, <br /> and
-                    they'll help you set up fast and easy.
+                    After opening your free account,
+                    <br /> the next thing is to select a plan
+                    <br /> that matches your budget, <br /> as well as your
+                    financial objectives. <br /> If you don't find a plan
+                    <br /> that suits your, just reach out <br /> to one of our
+                    available advisors, <br /> and they'll help you set up{" "}
+                    <br />
+                    fast and easy.
                   </p>
                 </div>
               </div>
-              <div
-                style={{ paddingTop: "2rem", paddingBottom: "1rem" }}
-                className="slide"
-              >
+              <div style={{ paddingBottom: "1rem" }} className="slide">
                 <div style={{}}>
-                  <img
-                    alt="profile-user"
-                    width="90px"
-                    height="90px"
-                    src={step03}
-                    style={{
-                      borderRadius: "50%",
-                      textAlign: "center",
-                      marginLeft: "47%",
-                      paddingBottom: "1rem",
-                    }}
-                  />
                   <h1
                     style={{ paddingBottom: "2rem" }}
                     className={styles.heading4}
@@ -365,10 +440,13 @@ const Invest = () => {
                   </h1>
 
                   <p className={styles.paragraph}>
-                    Funding your account has been made fast and easy. All you
-                    have to do is request the payment account and make the
-                    deposit. <br /> As soon as your deposit is confirmed, your
-                    profit starts to count.
+                    Funding your account
+                    <br /> has been made fast and easy.
+                    <br /> All you have to do is request
+                    <br /> the payment account and make
+                    <br /> the deposit. <br /> As soon as your deposit
+                    <br /> is confirmed, your profit <br />
+                    starts to count.
                   </p>
                 </div>
               </div>
